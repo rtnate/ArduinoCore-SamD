342715c02646df3902f1b2fb9bc4b5c39e6e5fec
nate <nate@rtelectronix.com>
2021-07-19 15:15:44 -0500

    Added rtelectronix_sc3_v3 variant Cloned from a featherM0 variant. sc3_pins.h contains hardware pin definitions. Removed Serial5 instance so it can be overrwritten with MIDI implementation.  Also removed default SERCOM5_Handler() implementation so it can be overridden by firmware. Added linker script support for the UserFlashArray to be stored at the end of the internal flash storage for use as program memory.

--- variants/rtelectronix_sc3_v3/sc3_pins.h
+++ variants/rtelectronix_sc3_v3/sc3_pins.h
@@ -5,29 +5,34 @@
 #error "This File (sc3_pins.h) Must Be Included From variant.h"
 #endif
 
-#define SC3_LINK_RX (0ul)
-#define SC3_LINK_TX (1ul)
-#define SC3_CS_SHIFT_REGISTERS (2ul)
-#define SC3_PWM_R (3ul)
-#define SC3_PWM_C (4ul)
-#define SC3_PWM_L (9ul)
-#define SC3_PRESET_OSC_ENABLE (11ul)
-#define SC3_MODE_SWITCH_PIN (5ul)
-#define SC3_TEMPO_MULT_PIN (8ul)
-#define SC3_MIX_PIN A5
-#define SC3_CHORUS_I_PIN A0
-#define SC3_CHORUS_II_PIN A1
-#define SC3_CHORUS_III_PIN A2
-#define SC3_SPEED_PIN A3
-#define SC3_WIDTH_PIN A4
-#define SC3_FSW_A_PIN (6ul)
-#define SC3_FSW_B_PIN (12ul)
-#define SC3_FSW_C_PIN (10ul)
-#define SC3_3V_LOWER (7ul)
-#define SC3_SDA_PIN PIN_WIRE_SDA
-#define SC3_SCL_PIN PIN_WIRE_SCL
-#define SC3_MOSI_SHIFT_REGISTERS PIN_SPI_MOSI
-#define SC3_SCK_SHIFT_REGISTERS PIN_SPI_SCK
-#define SC3_MIDI_TX (30ul)
-#define SC3_MIDI_RX (31ul)
+#define SC3_LINK_RX (0ul) //!< Pedal Link Serial Rx Pin
+#define SC3_LINK_TX (1ul) //!< Pedal Link Serial Tx Pin
+#define SC3_CS_SHIFT_REGISTERS (2ul) //!< Shift Registers Chip Select Pin
+#define SC3_PWM_R (3ul) //!< Control Voltage PWM Pin Right
+#define SC3_PWM_C (4ul) //!< Control Voltage PWM Pin Center
+#define SC3_PWM_L (9ul) //!< Control Voltage PWM Pin Left
+#define SC3_PRESET_OSC_ENABLE (11ul) //!< Analog Preset Oscillator Enable Pin
+#define SC3_MODE_SWITCH_PIN (5ul) //!< Mode Switch Input Pin
+#define SC3_TEMPO_MULT_PIN (8ul) //!< Tempo Mult Rotary Switch Input Pin
+#define SC3_MIX_PIN A5 //!< Mix Control Input Pin
+#define SC3_CHORUS_I_PIN A0 //!< Chorus I Control Input Pin
+#define SC3_CHORUS_II_PIN A1 //!< Chourus II Control Input Pin
+#define SC3_CHORUS_III_PIN A2 //!< Chorus III Control Input Pin
+#define SC3_SPEED_PIN A3 //!< Speed Control Input Pin
+#define SC3_WIDTH_PIN A4 //!< Width Control Input Pin
+#define SC3_FSW_A_PIN (6ul) //!< Footswitch A (Left) Input Pin
+#define SC3_FSW_B_PIN (12ul) //!< Footswitch B (Center) Input Pin
+#define SC3_FSW_C_PIN (10ul) //!< Footswitch C (Right) Input Pin
+#define SC3_3V_LOWER (7ul) //!< 3.3V To Lower Board Enable Pin
+#define SC3_SDA_PIN PIN_WIRE_SDA //!< I2C SDA Line Pin
+#define SC3_SCL_PIN PIN_WIRE_SCL //!< I2C SCL Line Pin
+#define SC3_MOSI_SHIFT_REGISTERS PIN_SPI_MOSI //!< Shift Registers Data Out Pin
+#define SC3_SCK_SHIFT_REGISTERS PIN_SPI_SCK //!< Shift Registers Clock Out Pin
+#define SC3_MIDI_TX (30ul) //!< MIDI Serial Tx Pin
+#define SC3_MIDI_RX (31ul) //!< MIDI Serial Rx Pin
+
+#define SC3_MIDI_PAD_RX (SERCOM_RX_PAD_3) //!< MIDI Serial Rx Sercom Pad
+#define SC3_MIDI_PAD_TX (UART_TX_PAD_2) //!< MIDI Serial Tx Sercom Pad
+#define SC3_MIDI_SERCOM sercom5 //!< MIDI Serial Sercom Module Number
+
 #endif 
